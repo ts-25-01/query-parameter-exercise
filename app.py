72de60b8fb12d747f6dd07a0d4b9c147e6b8ee3b
OlgaTkach1989 <olga.tkachuk378@gmail.com>
2025-05-12 13:55:25 +0200

    Übung 4: Filter-Parameter hinzugefügt

--- app.py
+++ app.py
@@ -37,6 +37,17 @@ def search():
     query = request.args.get('q', 'Keine Suchanfrage')
     sort_by = request.args.get('sort', 'Standardsortierung')
     return f"Suche nach: {query}, Sortierung: {sort_by}"
+
+
+@app.route('/produkte')
+def produkte():
+    kategorie = request.args.get('kategorie')
+
+    if kategorie:
+        return f"Zeige Produkte aus der Kategorie: {kategorie}"
+    else:
+        return "Zeige alle Produkte"
+
     
 # http://127.0.0.1:5000/search?s=python&sort=relevance
 if __name__ == "__main__":
